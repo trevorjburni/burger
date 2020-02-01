d36a653bfc732325a3b7e39c30289e8f36c6972c
Pilot <trevorjburni@gmail.com>
2020-02-01 16:06:54 -0700

    Coded out the controller.js file

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,3 +3,40 @@ var express = require("express");
 var burger = require("../models/burger.js");
 
 var router = express.Router();
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var handlebarsObject = {
+            burgers: data
+        };
+        console.log(handlebarsObject);
+        res.render("index", handlebarsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [req.body.burger_name, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
