0baf7863dcebba8e38ab5b9ec3052d72c48234ea
Pilot <trevorjburni@gmail.com>
2020-02-01 15:16:36 -0700

    Created connection.js file, orm.js file and updated package.json

---
+++ config/orm.js
@@ -0,0 +1,16 @@
+// import MySQL connection
+var connection = require("../config/connection.js");
+
+function selectAll() {
+
+}
+
+function insertOne() {
+
+}
+
+function updateOne() {
+
+}
+
+module.exports = orm;
