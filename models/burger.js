47171b96b901c072fcddde1d3fba1ecef95b579e
Pilot <trevorjburni@gmail.com>
2020-02-01 15:57:30 -0700

    Coded out the model/burger.js file, which will run code for the controller

--- models/burger.js
+++ models/burger.js
@@ -2,4 +2,22 @@ var orm = require("../config/orm.js");
 
 // Create code that will call the orm functions using burger specific input.
 
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
 module.exports = burger;
