ce6b01a2855a8bb71772cf72247aa1c560115485
Pilot <trevorjburni@gmail.com>
2020-02-01 19:11:16 -0700

    Corrected connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Pilot943foot81!",
-  database: "burgers_db"
+var connection;
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Pilot943foot81!",
+    database: "burgers_db"
 });
 
 // Make connection.
