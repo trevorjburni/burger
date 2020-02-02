6e756cba2587dd30b99ec632ab5c9470c9da5269
Pilot <trevorjburni@gmail.com>
2020-02-01 19:39:19 -0700

    A type was corrected in connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 
 var connection;
 
-if(process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
@@ -12,16 +12,16 @@ if(process.env.JAWSDB_URL) {
     user: "root",
     password: "Pilot943foot81!",
     database: "burgers_db"
-});
+  });
+}
+  // Make connection.
+  connection.connect(function (err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
-module.exports = connection;
+  // Export connection for our ORM to use.
+  module.exports = connection;
